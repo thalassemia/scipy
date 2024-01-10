29857fff7f068054a9e8d42d42da39d36e1e2829
thalassemia <67928790+thalassemia@users.noreply.github.com>
2024-01-09 19:29:10 -0800

    BUG: Use correct complex types in *ladiv

--- scipy/_build_utils/src/complex_blas.c
+++ scipy/_build_utils/src/complex_blas.c
@@ -1,5 +1,6 @@
 #include "fortran_defs.h"
 #include "npy_cblas.h"
+#include "npy_2_complexcompat.h"
 
 #ifdef F_SRC
 #include <complex.h>
@@ -39,16 +40,42 @@ double_complex F_FUNC(wzdotu, WZDOTU)(CBLAS_INT *n, double_complex *zx, CBLAS_IN
     return ret;
 }
 
+#ifdef F_SRC
 void BLAS_FUNC(sladiv)(float *xr, float *xi, float *yr, float *yi, float *retr, float * reti);
-void F_FUNC(wcladiv, WCLADIV)(void *ret, void *x, void *y){
+void F_FUNC(wcladiv, WCLADIV)(float_complex *ret, float_complex *x, float_complex *y){
     BLAS_FUNC(sladiv)((float*)(x), (float*)(x)+1, \
         (float*)(y), (float*)(y)+1, \
         (float*)(ret), (float*)(ret)+1);
 }
 
 void BLAS_FUNC(dladiv)(double *xr, double *xi, double *yr, double *yi, double *retr, double * reti);
-void F_FUNC(wzladiv, WZLADIV)(void *ret, void *x, void *y){
+void F_FUNC(wzladiv, WZLADIV)(double_complex *ret, double_complex *x, double_complex *y){
     BLAS_FUNC(dladiv)((double*)(x), (double*)(x)+1, \
         (double*)(y), (double*)(y)+1, \
         (double*)(ret), (double*)(ret)+1);
 }
+#else
+void BLAS_FUNC(sladiv)(float *xr, float *xi, float *yr, float *yi, float *retr, float * reti);
+void F_FUNC(wcladiv, WCLADIV)(float_complex *ret, float_complex *x, float_complex *y){
+    float zr, zi, xr, xi, yr, yi;
+    xr = npy_crealf(*x);
+    xi = npy_cimagf(*x);
+    yr = npy_crealf(*y);
+    yi = npy_cimagf(*y);
+    BLAS_FUNC(sladiv)(&xr, &xi, &yr, &yi, &zr, &zi);
+    NPY_CSETREALF(ret, zr);
+    NPY_CSETIMAGF(ret, zi);
+}
+
+void BLAS_FUNC(dladiv)(double *xr, double *xi, double *yr, double *yi, double *retr, double * reti);
+void F_FUNC(wzladiv, WZLADIV)(double_complex *ret, double_complex *x, double_complex *y){
+    double zr, zi, xr, xi, yr, yi;
+    xr = npy_creal(*x);
+    xi = npy_cimag(*x);
+    yr = npy_creal(*y);
+    yi = npy_cimag(*y);
+    BLAS_FUNC(dladiv)(&xr, &xi, &yr, &yi, &zr, &zi);
+    NPY_CSETREAL(ret, zr);
+    NPY_CSETIMAG(ret, zi);
+}
+#endif
