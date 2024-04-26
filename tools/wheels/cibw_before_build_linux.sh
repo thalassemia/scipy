b28bf72ddde8fa6c8cacd520fdf3a4c2fd8b6a85
Matti Picus <matti.picus@gmail.com>
2024-04-26 12:22:12 +1000

    restore part of script mistakenly removed

--- tools/wheels/cibw_before_build_linux.sh
+++ tools/wheels/cibw_before_build_linux.sh
@@ -1,6 +1,18 @@
 set -xe
 
 
+NIGHTLY_FLAG=""
+
+if [ "$#" -eq 1 ]; then
+    PROJECT_DIR="$1"
+elif [ "$#" -eq 2 ] && [ "$1" = "--nightly" ]; then
+    NIGHTLY_FLAG="--nightly"
+    PROJECT_DIR="$2"
+else
+    echo "Usage: $0 [--nightly] <project_dir>"
+    exit 1
+fi
+
 printenv
 # Update license
 cat $PROJECT_DIR/tools/wheels/LICENSE_linux.txt >> $PROJECT_DIR/LICENSE.txt
